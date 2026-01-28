a2383aa37da654797528895bd2899ee035858dab
Samuel Huang <samhuang91@gmail.com>
2026-01-28 05:04:18 -0800

    Remove JUnit 5 from deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,21 +19,13 @@ dependencies {
     implementation("org.apache.beam:beam-sdks-java-core:2.71.0")
     implementation("org.apache.beam:beam-runners-direct-java:2.71.0")
     implementation("ch.qos.logback:logback-classic:1.5.26")
-    testImplementation(platform("org.junit:junit-bom:5.14.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.hamcrest:hamcrest:3.0")
     testImplementation("org.apache.beam:beam-sdks-java-testing-junit:2.71.0")
-    testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 spotless {
     java {
         eclipse()
     }
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
